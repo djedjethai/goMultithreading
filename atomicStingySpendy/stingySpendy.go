b484c578b629370f0aa0fc56f9045dd4dacb9327
Jerome Bidault <djedje.thai.ok@gmail.com>
2021-09-18 14:00:51 +0700

    start atomic multithreading

---
+++ atomicStingySpendy/stingySpendy.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	money int32 = 100
+)
+
+func stingy() {
+	for i := 1; i <= 1000; i++ {
+		atomic.AddInt32(&money, 10)
+		fmt.Println("Stingy sees balance of: ", money)
+		time.Sleep(1 * time.Millisecond)
+	}
+	println("stingy done")
+}
+
+func spendy() {
+	for i := 1; i <= 1000; i++ {
+		atomic.AddInt32(&money, -10)
+		fmt.Println("Spendy sees balance of: ", money)
+		time.Sleep(1 * time.Millisecond)
+	}
+	println("spendy done")
+}
+
+func main() {
+	go stingy()
+	go spendy()
+	time.Sleep(3000 * time.Millisecond)
+	print(money)
+}
